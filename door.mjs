1969ccea14d94959af2f2e10f98903d5c12c85f3
Benn Bollay <benn@reachsuite.io>
2024-10-14 09:07:30 -0700

    .

---
+++ door.mjs
@@ -0,0 +1,64 @@
+const doorEndpoint = process.env.UNIFI_DOOR_API;
+const doorAuthToken = process.env.UNIFI_DOOR_TOKEN;
+
+const doorHeaders = {
+  Authorization: `Bearer ${doorAuthToken}`,
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+const fetchDoorOpenings = async (timeBracket) => {
+  const body = {
+    topic: 'door_openings',
+    since: Math.floor(timeBracket[0] / 1000),
+    until: Math.floor(timeBracket[1] / 1000),
+    timezone: 'America/Los_Angeles',
+  };
+  const result = await fetch(`${doorEndpoint}/api/v1/developer/system/logs/export`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { ...doorHeaders },
+  });
+
+  const text = await result.text();
+
+  const data = csv.parse(text, { columns: true });
+
+  const openings = {};
+  for await (const record of data) {
+    // Filter out failed entrance attempts (which don't have a user anyways)
+    if (record['event.result'] != 'ACCESS') {
+      continue;
+    }
+    const name = record['actor.display_name'];
+    if (name in openings) {
+      continue;
+    }
+    openings[name] = {
+      name,
+      doorId: record['actor.id'],
+      ...((await fetchDoorUser(record['actor.id'])) || {}),
+    };
+  }
+
+  return { data: Object.values(openings).reverse(), timeBracket };
+};
+
+const fetchDoorUser = async (userId) => {
+  const result = await fetch(`${doorEndpoint}/api/v1/developer/users/${userId}`, { headers: { ...doorHeaders } });
+
+  const res = await result.json();
+
+  if (res.code != 'SUCCESS') {
+    return undefined;
+  }
+
+  return {
+    doorEmail: res.data.user_email,
+    firstName: res.data.first_name,
+    lastName: res.data.last_name,
+    fullName: res.data.full_name,
+  };
+};
+
+export { fetchDoorOpenings };
---
+++ slack.mjs
@@ -0,0 +1,131 @@
+const slackOAuthToken = process.env.SLACK_OAUTH_BOT_TOKEN;
+const slackChannelId = process.env.SLACK_CHANNEL_ID;
+
+// These were experimentally identified by looking at response payloads.
+const slackLinkedInProfileId = 'Xf07RM6SNW3W';
+const slackBlurbProfileId = 'Xf07RC36BECE';
+
+const slackHeaders = { Authorization: `Bearer ${slackOAuthToken}`, 'content-type': 'application/json; charset=utf-8' };
+
+const fetchSlackUser = async (email) => {
+  if (!email) {
+    return undefined;
+  }
+  const url = new URL('https://slack.com/api/users.lookupByEmail');
+  url.searchParams.set('email', email);
+
+  const result = await fetch(url, { headers: { ...slackHeaders } });
+
+  const res = await result.json();
+
+  if (!res.ok) {
+    return undefined;
+  }
+
+  return { slackId: res.user.id, pictureUrl: res.user.profile.image_24 };
+};
+
+const fetchSlackProfile = async (userId) => {
+  if (!userId) {
+    return undefined;
+  }
+  const url = new URL('https://slack.com/api/users.profile.get');
+  url.searchParams.set('user', userId);
+
+  const result = await fetch(url, { headers: { ...slackHeaders } });
+
+  const res = await result.json();
+
+  const linkedInUrl = res.profile?.fields?.[slackLinkedInProfileId]?.value;
+  const details = res.profile?.fields?.[slackBlurbProfileId]?.value;
+
+  if (!res.ok) {
+    return undefined;
+  }
+
+  return { profileUrl: linkedInUrl, details };
+};
+
+const createSlackMessageBlock = async (time, users) => {
+  const when = new Date(time);
+  const whenStr = when.toLocaleTimeString('en-US');
+
+  const payload = {
+    blocks: [
+      {
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: `${whenStr}`,
+        },
+      },
+      ...users.map((user) => ({
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: ' -',
+          },
+          ...[
+            user.pictureUrl
+              ? {
+                  type: 'image',
+                  image_url: user.pictureUrl,
+                  alt_text: user.name,
+                }
+              : {},
+          ],
+          {
+            type: 'mrkdwn',
+            text: `${user.slackId ? `<@${user.slackId}>${user.profileUrl ? ` (<${user.profileUrl}|LI>)` : ''}` : user.name}${user.details ? `: ${user.details}` : ''}`,
+          },
+        ],
+      })),
+    ],
+  };
+
+  return payload;
+};
+
+const updateSlackMessage = async (ts, message) => {
+  const body = { channel: slackChannelId, ts, blocks: message.blocks };
+  const result = await fetch('https://slack.com/api/chat.update', {
+    method: 'POST',
+    headers: { ...slackHeaders },
+    body: JSON.stringify(body),
+  });
+
+  const res = await result.json();
+  if (!res.ok) {
+    console.log(JSON.stringify({ body, result: res }, null, 2));
+  }
+};
+
+const postSlackMessage = async (message) => {
+  const body = { channel: slackChannelId, blocks: message.blocks };
+  const result = await fetch('https://slack.com/api/chat.postMessage', {
+    method: 'POST',
+    headers: { ...slackHeaders },
+    body: JSON.stringify(body),
+  });
+
+  const res = await result.json();
+  if (!res.ok) {
+    console.log(JSON.stringify({ body, result: res }, null, 2));
+    return undefined;
+  }
+  return { ts: res.ts };
+};
+
+const getSlackStatus = () => JSON.parse(fs.readFileSync('doorlog.json', 'utf8'));
+const writeSlackStatus = (when, status) => fs.writeFileSync('doorlog.json', JSON.stringify({ [when]: status }));
+
+export {
+  getSlackStatus,
+  writeSlackStatus,
+  fetchSlackUser,
+  fetchSlackProfile,
+  createSlackMessageBlock,
+  updateSlackMessage,
+  postSlackMessage,
+};
